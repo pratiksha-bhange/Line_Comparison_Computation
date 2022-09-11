6fd1b43c4c1029d239a06a6216b9c0d1b828860b
pratiksha-bhange <pratikshabhange101@gmail.com>
2022-09-11 20:03:01 +0530

    [Pratu]ADDED: Check Equality Of Two Lines

--- LineComparison Computation/Program.cs
+++ LineComparison Computation/Program.cs
@@ -8,18 +8,27 @@ namespace LineComparison_Computation
         {
             Console.WriteLine("Welcome to Line Comparison Computation Program on Master Branch");
 
+            int x1, x2, y1, y2;
             Console.WriteLine("Enter the value of (x1,y1),(x2,y2) cordinates");
-            int x1 = Convert.ToInt32(Console.ReadLine());
-            int x2 = Convert.ToInt32(Console.ReadLine());
-            int y1 = Convert.ToInt32(Console.ReadLine());
-            int y2 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine(toFindLength(x1, y1, x2, y2));
+            x1 = Convert.ToInt32(Console.ReadLine());
+            x2 = Convert.ToInt32(Console.ReadLine());
+            y1 = Convert.ToInt32(Console.ReadLine());
+            y2 = Convert.ToInt32(Console.ReadLine());
+            int length1 = toFindLength(x1, y1, x2, y2);
+
+            x1 = Convert.ToInt32(Console.ReadLine());
+            x2 = Convert.ToInt32(Console.ReadLine());
+            y1 = Convert.ToInt32(Console.ReadLine());
+            y2 = Convert.ToInt32(Console.ReadLine());
+            int length2 = toFindLength(x1, y1, x2, y2);
+
+            Console.WriteLine(length1.Equals(length2));
 
         }
 
         private static int toFindLength(int x1, int y1, int x2, int y2)
         {
-            return (int)Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+            return (int)Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
         }
     }
 }
