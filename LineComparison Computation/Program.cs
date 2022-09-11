381909a5ac2d4110e1e9393b92c2202b605f684a
pratiksha-bhange <pratikshabhange101@gmail.com>
2022-09-11 20:06:49 +0530

    [Pratu]ADDED: Compare Two Lines

--- LineComparison Computation/Program.cs
+++ LineComparison Computation/Program.cs
@@ -6,8 +6,6 @@ namespace LineComparison_Computation
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Line Comparison Computation Program on Master Branch");
-
             int x1, x2, y1, y2;
             Console.WriteLine("Enter the value of (x1,y1),(x2,y2) cordinates");
             x1 = Convert.ToInt32(Console.ReadLine());
@@ -15,20 +13,24 @@ namespace LineComparison_Computation
             y1 = Convert.ToInt32(Console.ReadLine());
             y2 = Convert.ToInt32(Console.ReadLine());
             int length1 = toFindLength(x1, y1, x2, y2);
+            Console.WriteLine(length1);
 
             x1 = Convert.ToInt32(Console.ReadLine());
             x2 = Convert.ToInt32(Console.ReadLine());
             y1 = Convert.ToInt32(Console.ReadLine());
             y2 = Convert.ToInt32(Console.ReadLine());
             int length2 = toFindLength(x1, y1, x2, y2);
+            Console.WriteLine(length2);
 
             Console.WriteLine(length1.Equals(length2));
+
+            Console.WriteLine("Return value (comparison) = " + length1.CompareTo(length2));
 
         }
 
         private static int toFindLength(int x1, int y1, int x2, int y2)
         {
-            return (int)Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
+            return (int)Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
         }
     }
 }
